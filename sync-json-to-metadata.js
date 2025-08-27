fa921e4e5b3cf73b7a383f4f447e7977395d7a3a
jaspreetsinghngh@gmail.com <jaspreetsinghngh@gmail.com>
2025-08-27 23:05:05 +0530

    added files

---
+++ sync-json-to-metadata.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+// Input/Output paths
+const jsonFile = path.join(__dirname, "../additional-files/my_record.json");
+const metadataFile = path.join(__dirname, "../force-main/main/default/customMetadata/JSON_HOLDER.TEST.md-meta.xml");
+
+// Read JSON
+const jsonData = JSON.parse(fs.readFileSync(jsonFile, "utf8"));
+
+// Convert JSON to Metadata XML
+const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
+<CustomMetadata xmlns="http://soap.sforce.com/2006/04/metadata" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <label>Test</label>
+    <protected>false</protected>
+    <values>
+        <field>Value__c</field>
+        <value xsi:type="xsd:string">${jsonData}</value>
+    </values>
+</CustomMetadata>`;
+
+// Write output
+fs.writeFileSync(metadataFile, xmlContent, "utf8");
