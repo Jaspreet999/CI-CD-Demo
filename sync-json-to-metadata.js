2c29f12678d9cc7df197a11200794fd764e10d6a
Jaspreet999 <jaspreetsinghngh@gmai.com>
2025-08-28 08:55:45 +0530

    updates

--- sync-json-to-metadata.js
+++ sync-json-to-metadata.js
@@ -3,7 +3,6 @@ const path = require("path");
 
 // Input/Output paths
 const jsonFile = path.join(process.cwd(), "files", "my_record.json");
-
 const metadataFile = path.join(__dirname, "../force-app/main/default/customMetadata/JSON_HOLDER.TEST.md-meta.xml");
 const jsonData = JSON.parse(fs.readFileSync(jsonFile, "utf8"));
 
